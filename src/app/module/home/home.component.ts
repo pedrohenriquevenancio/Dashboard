aa20394565969b0ae8d8bc317b04df22b5855199
Pedro Venâncio <ph.venancio2@gmail.com>
2024-12-04 18:40:58 -0300

    style: add sidebar

--- src/app/module/home/home.component.ts
+++ src/app/module/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeSideBarComponent } from './home-side-bar/home-side-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    HomeSideBarComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
